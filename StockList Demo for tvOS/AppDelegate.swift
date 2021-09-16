b03b6342015ec1cbfff96cd04a5e29da0380511a
acarioni <alessandro.carioni@lightstreamer.com>
2021-09-16 08:26:56 +0200

    convert AppDelegate

---
+++ StockList Demo for tvOS/AppDelegate.swift
@@ -0,0 +1,43 @@
+//  Converted to Swift 5.4 by Swiftify v5.4.25812 - https://swiftify.com/
+//
+//  AppDelegate.swift
+//  StockList Demo for tvOS
+//
+//  Copyright © 2016 Lightstreamer. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    // MARK: -
+    // MARK: Properties
+    var window: UIWindow?
+
+    // MARK: -
+    // MARK: App life cycle
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+    }
+}
+
+// MARK: -
+// MARK: AppDelegate extension
+// MARK: -
+// MARK: AppDelegate implementation
